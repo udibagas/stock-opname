0ff79e9527fc0261264a63a76a9ecac4eb222bfc
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-04-19 09:11:51 +0700

    change output dir

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "../www",
-    emptyOutDir: true,
-  },
+  // build: {
+  //   outDir: "../www",
+  //   emptyOutDir: true,
+  // },
 });
